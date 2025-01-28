987e70b7ec24c5e96f7fd9686c0a0a2a5cc727b8
Pallavi <git@123.in>
2025-01-28 09:24:37 +0530

    for loop

--- demo2/src/main.rs
+++ demo2/src/main.rs
@@ -2,4 +2,8 @@ fn main() {
     let vec = vec![1,2,3,4,5];
     println!("My vec is :{:?}",vec);
     println!("Hello there, world!");
+
+    for i in 1..10{
+        println!("Current iterator is : {}",i);
+    }
 }
